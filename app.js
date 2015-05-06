4892dc5fdaa1f5aaf51c3528fbd1a5ded38fd370
DrewMcArthur <drewmcarthur1@gmail.com>
2015-05-06 19:16:37 -0400

    added root changes to prevent errors without sudo, and removes root priveleges after listening on port 80.  see http://syskall.com/dont-run-node-dot-js-as-root/ for more info on this change

--- app.js
+++ app.js
@@ -16,9 +16,25 @@ app.get('/',function(req,res){
 	res.sendFile(__dirname + '/index.html');
 });//this hosts the ./index.html file when '/' is requested.
 
-app.locals.port = 80; //change this to change port to listen to.
+//check if node was run with sudo
+if(process.env.USERNAME == 'root')
+	app.locals.port = 80;
+else //if node wasn't run as root, host server on :8000
+	app.locals.port = 8000;
 
 //listen for requests at ipaddress:port specified at :19
 app.listen(app.locals.port, function(){ 
 	console.log("Server is running on port " + app.locals.port);
+
+	if(process.env.USERNAME == 'root'){
+		// code snippet to change user after port is listened to for security, 
+		// found at http://syskall.com/dont-run-node-dot-js-as-root/
+
+		// Find out which user used sudo through the environment variable
+		var uid = parseInt(process.env.SUDO_UID);
+		// Set our server's uid to that user
+		if (uid) process.setuid(uid);
+		console.log('Server\'s UID is now ' + process.getuid());
+	} // end if root
+
 });
