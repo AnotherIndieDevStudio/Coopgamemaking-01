4191b2150845f2d3610a9c2f7c39209065a1bfb1
DrewMcArthur <drewmcarthur1@gmail.com>
2015-05-06 18:31:09 -0400

    add correct hosting for middleware. this successfully hosts the server in its entirety so far.

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/'));//this hosts the files located in the ./public directory
+//this hosts the files located in the ./js directory;
+app.use('/js', express.static(__dirname + '/js'));
+//this hosts the files located in the ./style directory
+app.use('/style', express.static(__dirname + '/style'));
+//this hosts the files located in the ./public directory
+app.use(express.static(__dirname + '/public'));
 
-app.locals.port = 80;
+// I find that app.use is better to host a directory, 
+// however I didn't want to host the entire '/' of the project, 
+// since I don't know if this is risky. 
+// :13-15 is a workaround without moving the index file.
+app.get('/',function(req,res){ 
+	res.sendFile(__dirname + '/index.html');
+});//this hosts the ./index.html file when '/' is requested.
 
-app.listen(app.locals.port, function(){ //listen for requests at ipaddress:1337
-	console.log("Server is running on port " + app.locals.port);		//callback function, completely optional.
+app.locals.port = 80; //change this to change port to listen to.
+
+//listen for requests at ipaddress:port specified at :19
+app.listen(app.locals.port, function(){ 
+	console.log("Server is running on port " + app.locals.port);
 });
