c01d9e5cf7dfa26e76f10cf9812a92d54900d39a
DrewMcArthur <drewmcarthur1@gmail.com>
2015-05-06 18:10:38 -0400

    removed testing console.log

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 app.use(express.static(__dirname + '/'));//this hosts the files located in the ./public directory
 
 app.locals.port = 80;
-console.log(app.locals.port);
+
 app.listen(app.locals.port, function(){ //listen for requests at ipaddress:1337
 	console.log("Server is running on port " + app.locals.port);		//callback function, completely optional.
 });
