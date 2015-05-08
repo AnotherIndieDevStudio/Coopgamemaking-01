511895fedef96e826628ae846eb26e5b2da9a39b
Alex Bickford <a.bickford@paltd.net>
2015-05-08 09:45:56 +0100

    Updated upgrade code
    Re-wrote so that it was less lines and easier to edit

--- js/game.js
+++ js/game.js
@@ -1,40 +1,40 @@
 (function () {
-	
+
 	var Game = window.Game = window.Game || {};
-	
+
 	if (!Game.player) {
-		
+
 		// Initialise Player on load to a character with default stats
 		Game.player = Game.Character();
-		
+
 	}
-	
-	
+
+
 	if (!Game.time) {
-		
+
 		// Initialise Game.Time
 		Game.time = {
 			frequency: 250,
 			previous: new Date(),
 			passed: 0
 		};
-		
+
 	}
-	
+
 
 	/* Updating variables on page when the document loads */
 	$(document).ready(function (e) {
-	
+
 		// Initiate stats on screen
 		update_stats();
-	
+
 		// Game Loop
 		var game_tick = setInterval(function () {
-			
+
 			// Ensures that the game still functions when the tab is closed
 			var current_time = new Date();
 			var time_difference = (current_time.getTime() - Game.time.previous.getTime());
-			
+
 			// Calculates the difference in time and returns multiplier
 			if (time_difference > Game.time.frequency) {
 
@@ -53,17 +53,17 @@
 				Game.time.passed = 1;
 
 			}
-			
+
 			Game.time.previous = new Date();
 			update_game();
 
 		}, Game.time.frequency);
 
 	});
-	
+
 
 	var update_stats = function () {
-		
+
 		// Updating stats
 		$("#stat_points").html(Game.player.stat_points);
 		$("#level").html(Game.player.level);
@@ -76,13 +76,13 @@
 		$("#exp_tnl").html(Game.player.experience_tnl);
 
 	};
-	
+
 
 	var update_game = function () {
-		
+
 		// Adds exp each game tick
 		Game.player.experience += 10 * Game.time.passed;
-		
+
 		if (Game.player.experience > Game.player.experience_tnl) {
 
 			Game.player.experience -= Game.player.experience_tnl;
@@ -92,21 +92,21 @@
 			Game.player.max_stat_points += 2;
 
 		}
-	
+
 		// Updates stats on the screen
 		update_stats();
-	
+
 		// Checks if stats can be upgraded
 		check_player_can_level();
 
 	};
-	
-	
+
+
 	// Checks if stats can be upgraded and shows + symbols if player can level stats
 	var check_player_can_level = function () {
-		
+
 		Game.Character.check_can_level_stats(Game.player);
-		
+
 		if (Game.player.can_level_stats) {
 
 			$(".add_stat").show();
@@ -116,62 +116,23 @@
 			$(".add_stat").hide();
 
 		}
-			
+
 	};
-	
+
 	/* Checks for clicks on stat upgrades */
-
-	// Health
-	$("#health_add").click(function () {
-
+	$(".add_stat").click(function () {
+		/* Check button's data-stat */
+		var stat = $(this).data('stat');
 		if (Game.player.can_level_stats) {
-
-			Game.player.health += 10;
-			Game.player.max_health = Game.player.health;
+			/* if stat is 'health', update by 10, if not, then 1 */
+			Game.player[stat] += (stat === "health") ? 10 : 1;
+			/* Setting player's max health */
+			if (stat === "health") {
+				Game.player.max_health = Game.player.health;
+			}
 			Game.player.stat_points -= 1;
 			check_player_can_level();
-
 		}
-
-	});
-
-	// Strength
-	$("#strength_add").click(function () {
-
-		if (Game.player.can_level_stats) {
-
-			Game.player.strength += 1;
-			Game.player.stat_points -= 1;
-			check_player_can_level();
-
-		}
-
-	});
-
-	//Intellect
-	$("#intellect_add").click(function () {
-
-		if (Game.player.can_level_stats) {
-
-			Game.player.intellect += 1;
-			Game.player.stat_points -= 1;
-			check_player_can_level();
-
-		}
-
-	});
-
-	//Dexterity
-	$("#dexterity_add").click(function () {
-
-		if (Game.player.can_level_stats) {
-
-			Game.player.dexterity += 1;
-			Game.player.stat_points -= 1;
-			check_player_can_level();
-
-		}
-
 	});
 
 } ());
