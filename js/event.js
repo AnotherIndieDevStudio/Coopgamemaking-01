8081b725123a94c0ef044ad61b52d995d20cee89
Goodpie <brandynbb96@gmail.com>
2015-05-11 09:26:43 +0800

    Compulsory commit
    Won't let me switch branch if I don't commit

--- js/event.js
+++ js/event.js
@@ -1,7 +1,8 @@
 (function () {
-	
+
 	var Game = window.Game = window.Game || {};
-	
+
+
 	/**
 	 * An Event
 	 * @struct
@@ -9,7 +10,7 @@
 	 * @return {Event}
 	 */
 	Game.Event = function (obj) {
-		
+
 		var event = {
 			// Don't copy another events id. All Event instances should have a unique id.
 			id: /** {string} */Game.Math.UUID(),
@@ -17,68 +18,87 @@
 			time: /** {number} */(!obj || !obj.time) ? (Game.time ? Game.time.elapsed : 0) : obj.time,
 			description: /** {string} */(!obj || !obj.description) ? 'This event has no description' : obj.description
 		};
-		
+
+		// Events hidden
+		Game.Event.events_hidden = false;
+
+		// Events missed while events were hidden
+		Game.Event.event_count = 0;
+
 		// Index event by_id
 		Game.Event.by_id[event.id] = event;
-		
+
 		// Index event by_type
 		if (!Game.Event.by_type.hasOwnProperty(event.type)) {
-			
+
 			Game.Event.by_type[event.type] = [];
-			
+
 		}
-		
+
 		Game.Event.by_type[event.type].push(event);
-		
+
 		// Index event by_time
 		if (!Game.Event.by_time.hasOwnProperty(event.time)) {
-			
+
 			Game.Event.by_time[event.time] = [];
-			
+
 		}
-		
+
 		Game.Event.by_time[event.time].push(event);
-		
+
 		return event;
-		
+
 	};
-	
-	
-	// Events by id 
+
+
+	// Events by id
 	Game.Event.by_id = {};
-	
+
 	// Events by type
 	Game.Event.by_type = {};
-	
+
 	// Events by time
 	Game.Event.by_time = {};
-	
-	
-	
+
+
+
 	/**
 	 * Adds the event bubble to the timeline.
-	 * 
+	 *
 	 * @param {Event} event
 	 */
 	Game.Event.add_to_timeline = function (event) {
-		
+
 		var $bubble = $([
 			'<div class="event_bubble">',
 			'    <div class="event_time">t+' + event.time + '</div>',
 			'    <div class="event_description">' + event.description + '</div>',
 			'</div>'
 		].join('\n'));
-			
+
 		$("#events").prepend($bubble);
-		
+
 		$bubble.animate({marginTop: 10, opacity: 1}, 1000);
-		
+
+		// Updates the notification count when notifications are hidden
+		if(Game.Event.events_hidden){
+				Game.Event.event_count += 1;
+				$("#event-count").html("(" + Game.Event.event_count + ")");
+				$(".event_bubble").hide();
+		}
+
+		// Checks if there are too many notifications. If so, remove the last one.
+		if($("#event-toggle").position().top+$('#event-toggle').outerHeight(true) > $( window ).height() - 100){
+			$(".event_bubble:last").fadeOut(function(){
+				$(this).remove();
+			});
+		}
+
 	};
-	
-	
-	
+
+
 	Game.Events = {};
-	
+
 	Game.Events.events = [
 		Game.Event({type: 'environment', description: 'The smell of a sweet chicken broth circles about'}),
 		Game.Event({type: 'environment', description: 'The streets grow quieter as dusk approaches'}),
@@ -90,34 +110,34 @@
 		Game.Event({type: 'exchange', description: 'Growing weary, you find the closest tavern, take a load off your feet and engage the old bartender in conversation'}),
 		Game.Event({type: 'exchange', description: 'A pedler of foreign appearances rattles his staff as he hobbles on by'})
 	];
-	
-	
+
+
 	Game.Events.event_frequency_max = 20;
 	Game.Events.event_frequency_min = 200;
-	
+
 	Game.Events.next_event = Game.Event({type: 'information', description: 'Welcome to CoopGameMaking'});
-	
+
 	/**
 	 * Updates the events timeline.
 	 */
 	Game.Events.update = function () {
-		
+
 		if (Game.time.elapsed >= Game.Events.next_event.time) {
-			
+
 			Game.Event.add_to_timeline(Game.Events.next_event);
-			
+
 			var events_index = ~~(Math.random() * Game.Events.events.length);
-			
+
 			if (events_index === Game.Events.events.length) {
 				--events_index;
 			}
-			
+
 			Game.Events.next_event = Game.Events.events[events_index];
 			Game.Events.next_event.time = Game.time.elapsed + Game.Events.event_frequency_max +
 				~~(Math.random() * (Game.Events.event_frequency_min - Game.Events.event_frequency_max));
-			
+
 		}
-		
+
 	};
-	
+
 } ());
