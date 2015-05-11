7e8d42ba38a7de7d6d8abaa28a83e0df8b61bb0d
Tribuadore <stephencarmody+github@gmail.com>
2015-05-11 16:41:51 +1000

    aa

--- js/event.js
+++ js/event.js
@@ -10,7 +10,7 @@
 	 */
 	Game.Event = function (obj) {
 		
-		var event = {
+		return {
 			// Don't copy another events id. All Event instances should have a unique id.
 			id: /** {string} */Game.Math.UUID(),
 			type: /** {string} */(!obj || !obj.type) ? 'undefined' : obj.type,
@@ -18,60 +18,27 @@
 			description: /** {string} */(!obj || !obj.description) ? 'This event has no description' : obj.description
 		};
 		
-		// Index event by_id
-		Game.Event.by_id[event.id] = event;
-		
-		// Index event by_type
-		if (!Game.Event.by_type.hasOwnProperty(event.type)) {
-			
-			Game.Event.by_type[event.type] = [];
-			
-		}
-		
-		Game.Event.by_type[event.type].push(event);
-		
-		// Index event by_time
-		if (!Game.Event.by_time.hasOwnProperty(event.time)) {
-			
-			Game.Event.by_time[event.time] = [];
-			
-		}
-		
-		Game.Event.by_time[event.time].push(event);
-		
-		return event;
-		
 	};
 	
 	
-	// Events by id 
-	Game.Event.by_id = {};
-	
-	// Events by type
-	Game.Event.by_type = {};
-	
-	// Events by time
-	Game.Event.by_time = {};
-	
+	// Events to be fired, queued by time 
+	Game.Event.queued_by_time = {};
 	
 	
 	/**
-	 * Adds the event bubble to the timeline.
+	 * Queues an Event to be fired when its time is earlier and at the current Game elapsed time.
 	 * 
 	 * @param {Event} event
 	 */
-	Game.Event.add_to_timeline = function (event) {
+	Game.Event.queue = function (event) {
 		
-		var $bubble = $([
-			'<div class="event_bubble">',
-			'    <div class="event_time">t+' + event.time + '</div>',
-			'    <div class="event_description">' + event.description + '</div>',
-			'</div>'
-		].join('\n'));
+		if (!Game.Event.queued_by_time.hasOwnProperty(event.time)) {
 			
-		$("#events").prepend($bubble);
+			Game.Event.queued_by_time[event.time] = [];
+			
+		}
 		
-		$bubble.animate({marginTop: 10, opacity: 1}, 1000);
+		Game.Event.queued_by_time[event.time].push(event);
 		
 	};
 	
@@ -92,29 +59,72 @@
 	];
 	
 	
-	Game.Events.event_frequency_max = 20;
-	Game.Events.event_frequency_min = 200;
+	// The frequency range (in Game.time.elapsed ticks) of which above events are picked to fire
+	Game.Events.event_frequency_max = 20;		// Minimum elapsed ticks till next event
+	Game.Events.event_frequency_min = 200;		// Maximum elapsed ticks till next event
 	
-	Game.Events.next_event = Game.Event({type: 'information', description: 'Welcome to CoopGameMaking'});
+	
+	// Queue the very first Event to be an informational welcome message
+	Game.Event.queue(Game.Event({type: 'information', description: 'Welcome to CoopGameMaking', on_event: function (event) {
+		
+		var next_event = Game.Events.events[~~(Math.random() * Game.Events.events.length - 0.1)];
+		next_event.time = event.time + Game.Events.event_frequency_max +
+				~~(Math.random() * (Game.Events.event_frequency_min - Game.Events.event_frequency_max));
+		Game.Event.queue(next_event); 
+		
+	}}));
+	
 	
 	/**
 	 * Updates the events timeline.
 	 */
 	Game.Events.update = function () {
 		
-		if (Game.time.elapsed >= Game.Events.next_event.time) {
+		// Find queued events that are ready to fire at or before current elapsed time and sort them by time
+		var ordered_events_to_fire = [];
+		var queued_event_times = Object.getOwnPropertyNames(Game.Event.queued_by_time);
+		
+		for (var queued_event_time in queued_event_times) {
 			
-			Game.Event.add_to_timeline(Game.Events.next_event);
-			
-			var events_index = ~~(Math.random() * Game.Events.events.length);
-			
-			if (events_index === Game.Events.events.length) {
-				--events_index;
+			if (queued_event_time <= Game.time.elapsed) {
+				
+				var ordered_index = 0;
+				
+				while (ordered_index < ordered_index.length && ordered_events_to_fire[0].time < queued_event_time) {
+					
+					++ordered_index;
+					
+				}
+				
+				for (var event in Game.Event.queued_by_time[queued_event_time]) {
+					
+					ordered_events_to_fire.splice(ordered_index, event);
+					
+				}
+				
 			}
 			
-			Game.Events.next_event = Game.Events.events[events_index];
-			Game.Events.next_event.time = Game.time.elapsed + Game.Events.event_frequency_max +
-				~~(Math.random() * (Game.Events.event_frequency_min - Game.Events.event_frequency_max));
+		}
+		
+		// Iterate events that are ready to be fired 
+		for (var event in ordered_events_to_fire) {
+					
+			event = ordered_events_to_fire[event];
+			
+			// Add the Event bubble to the timeline
+			var $bubble = $([
+				'<div class="event_bubble">',
+				'    <div class="event_time">t+' + event.time + '</div>',
+				'    <div class="event_description">' + event.description + '</div>',
+				'</div>'
+			].join('\n'));
+			
+			$("#events").prepend($bubble);
+			
+			$bubble.animate({marginTop: 10, opacity: 1}, 1000);
+			
+			// Execute the events on_event function
+			event.on_event(event);
 			
 		}
 		
