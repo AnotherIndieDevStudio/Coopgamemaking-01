4acb93008297953dad253f41fd8c3953f4ba151a
Goodpie <brandynbb96@gmail.com>
2015-05-15 16:57:34 +0800

    Modified homepage to display defence.

--- js/items.js
+++ js/items.js
@@ -12,16 +12,17 @@
 	Game.Item = function(obj) {
 		
 		var item = {
-			id: /** {number} **/ (!obj || !obj.id) ? 0 : obj.id,
-			name: /** {string} **/ (!obj || !obj.name) ? "Nameless" : obj.name,
-			type: /** {string} **/ (!obj || !obj.type) ? "undefined" : obj.type,
-			description: /** {string} **/ (!obj || !obj.description) ? "No Description" : obj.description,
+			id: /** {number} **/ (!obj || !obj.id) ? '0' : obj.id,
+			name: /** {string} **/ (!obj || !obj.name) ? 'Nameless' : obj.name,
+			type: /** {string} **/ (!obj || !obj.type) ? 'undefined' : obj.type,
+			description: /** {string} **/ (!obj || !obj.description) ? 'No Description' : obj.description,
 			// Attributes are changes to the characters stats
 			attributes: {
 				health: /** {number} **/ (!obj || !obj.attributes.health) ? 0 : obj.attributes.health,
 				strength: /** {number} **/ (!obj || !obj.attributes.strength) ? 0 : obj.attributes.strength,
 				dexterity:  /** {number} **/ (!obj || !obj.attributes.dexterity) ? 0 : obj.attributes.dexterity,
 				intellect: /** {number} **/ (!obj || !obj.attributes.intellect) ? 0 : obj.attributes.intellect,
+				defence: /** {number} **/ (!obj || !obj.attributes.defence) ? 0 : obj.attributes.defence
 			}
 		};
 		
@@ -32,18 +33,24 @@
 		
 		Game.Item.by_type[item.type].push(item);
 		
+		if(!Game.Item.by_id.hasOwnProperty(item.id)){
+			Game.Item.by_id[item.id] = [];
+		}
+		
+		Game.Item.by_id[item.id].push(item);
+		
 		return item;
 	
 	};
 	
 	Game.Item.by_type = {};
+	Game.Item.by_id = {};
 	
 	Game.Items = {};
 	/* Some predefined items */
 	Game.Items.items = [
-		Game.Item({id: 1, name: "Hat", type: "Armour", description: "A simple hat", attributes: {health: 5, strength: 1, dexterity: 0, intellect: 1}}),
-		Game.Item({id: 2, name: "Wooden Sword", type: "Weapon", description: "A weak training sword", attributes: {health: 0, strength: 10, dexterity: 3, intellect: 0}})
+		Game.Item({id: '1', name: 'Hat', type: 'Armour', description: 'A simple hat', attributes: {health: 5, strength: 1, dexterity: 0, intellect: 1, defence: 2}}),
+		Game.Item({id: '2', name: 'Wooden Sword', type: 'Weapon', description: 'A weak training sword', attributes: {health: 0, strength: 10, dexterity: 3, intellect: 0, defence: 0}})
 	];
-	
 	
 } ());
